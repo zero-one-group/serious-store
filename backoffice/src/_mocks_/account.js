c9b5139f1289a2647c20c95d60df3e09b2e0c1cd
khoerul umam <khoerul@zero-one-group.com>
2021-11-09 16:37:09 +0700

    adjust folder and file

--- backoffice/src/_mocks_/account.js
+++ backoffice/src/_mocks_/account.js
@@ -1,8 +1,8 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
+  displayName: 'Khoerul Umam',
+  email: 'khoerul@serious.store',
   photoURL: '/static/mock-images/avatars/avatar_default.jpg'
 };
 
